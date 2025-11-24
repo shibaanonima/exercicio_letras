1064f89b35a2336ff8837817b5c8b4d1ebda28fc
Sofia <shibaanonima@gmail.com>
2025-11-24 18:29:30 -0300

    Add files via upload

---
+++ palavra2/programa/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite quantas letras tem a palavra que você quer escrever: ");
+            int x = int.Parse(Console.ReadLine());
+
+            char[] letra = new char[x];
+            string palavracompleta="";
+            int i;
+            string palavra = "";
+
+            for (i = 0; i < x; i++)
+            {
+                Console.Write("Digite a {0} letra : ", i + 1);
+                letra[1] = char.Parse(Console.ReadLine());
+                palavra = palavra + letra[i];
+                palavracompleta = palavra;
+                Console.WriteLine(palavracompleta);
+
+            }
+
+            Console.WriteLine();
+
+            for (i = 0; i < x; ++i)
+            {
+                Console.WriteLine("{0} letra: {1} ", i + 1, letra[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
