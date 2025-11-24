1602020ab6a820f482b66808d5de1a33c5228aab
Sofia <shibaanonima@gmail.com>
2025-11-24 18:28:55 -0300

    Add files via upload

---
+++ palavra1/palavra1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavracompleta = "";
+            string palavra = "";
+            char[] letra = new char[10];
+            int i;
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("Digite a {0} letra:", i + 1);
+                letra[i] = char.Parse(Console.ReadLine());
+                palavra = palavra + letra[i];
+                palavracompleta = palavra;
+                Console.WriteLine(palavracompleta);
+            }
+
+            Console.WriteLine();
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0} letra:{1} ", i + 1, letra[i]);
+            }
+
+            Console.WriteLine(palavracompleta);
+            Console.ReadKey();
+
+        }
+    }
+}
